6acad423003f80b209b62ccff9454ef7f9feb2bb
Somesh Sahu <sahusomesh77@gmail.com>
2026-07-21 16:54:48 +0530

    Configure GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,36 +1,160 @@
+// import { themes as prismThemes } from 'prism-react-renderer';
+// import type { Config } from '@docusaurus/types';
+// import type * as Preset from '@docusaurus/preset-classic';
+
+// // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+// const config: Config = {
+//   title: 'Aadhaar Authentication API Documentation',
+//   tagline: 'Full guide for integration of aadhaar authentication',
+//   favicon: 'img/favicon.ico',
+
+//   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+//   future: {
+//     v4: true, // Improve compatibility with the upcoming Docusaurus v4
+//   },
+
+//   // Set the production url of your site here
+//   url: 'https://ss11051999.github.io',
+//   // Set the /<baseUrl>/ pathname under which your site is served
+//   // For GitHub pages deployment, it is often '/<projectName>/'
+//   baseUrl: '/aadhaar_auth_docs/',
+
+//   // GitHub pages deployment config.
+//   // If you aren't using GitHub pages, you don't need these.
+//   organizationName: 'ss11051999', // Usually your GitHub org/user name.
+//   projectName: 'aadhaar_auth_docs', // Usually your repo name.
+
+//   trailingSlash: false,
+//   onBrokenLinks: 'throw',
+
+//   // Even if you don't use internationalization, you can use this field to set
+//   // useful metadata like html lang. For example, if your site is Chinese, you
+//   // may want to replace "en" with "zh-Hans".
+//   i18n: {
+//     defaultLocale: 'en',
+//     locales: ['en'],
+//   },
+
+//   presets: [
+//     [
+//       'classic',
+//       {
+//         docs: {
+//           sidebarPath: './sidebars.ts',
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//         },
+//         blog: false,
+//         theme: {
+//           customCss: './src/css/custom.css',
+//         },
+//       } satisfies Preset.Options,
+//     ],
+//   ],
+
+//   themeConfig: {
+//     // Replace with your project's social card
+//     image: 'img/docusaurus-social-card.jpg',
+//     colorMode: {
+//       respectPrefersColorScheme: true,
+//     },
+//     navbar: {
+//       title: 'Aadhaar Authentication Documentation',
+//       logo: {
+//         alt: 'My Site Logo',
+//         src: 'img/aadhaar_chips_logo.png',
+//       },
+//       items: [
+//         {
+//           type: 'docSidebar',
+//           sidebarId: 'tutorialSidebar',
+//           position: 'left',
+//           label: 'Tutorial',
+//         },
+//         {
+//           href: 'https://github.com/facebook/docusaurus',
+//           label: 'GitHub',
+//           position: 'right',
+//         },
+//       ],
+//     },
+//     footer: {
+//       style: 'dark',
+//       links: [
+//         {
+//           title: 'Docs',
+//           items: [
+//             {
+//               label: 'Tutorial',
+//               to: '/docs/intro',
+//             },
+//           ],
+//         },
+//         {
+//           title: 'Community',
+//           items: [
+//             {
+//               label: 'CHiPS Official Site',
+//               href: 'https://www.chips.gov.in/en',
+//             },
+//             {
+//               label: 'Aadhaar',
+//               href: 'https://uidai.gov.in/en/',
+//             },
+//           ],
+//         },
+//         {
+//           title: 'More',
+//           items: [
+//             {
+//               label: 'GitHub',
+//               href: 'https://github.com/facebook/docusaurus',
+//             },
+//           ],
+//         },
+//       ],
+//       copyright: `Copyright © ${new Date().getFullYear()} CHiPS`,
+//     },
+//     prism: {
+//       theme: prismThemes.github,
+//       darkTheme: prismThemes.dracula,
+//     },
+//   } satisfies Preset.ThemeConfig,
+// };
+
+// export default config;
+
+
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
-  title: 'Aadhaar Authentication Documentation',
-  tagline: 'Full guide for integration of aadhaar authentication',
+  title: 'Aadhaar Authentication API Documentation',
+  tagline: 'Full guide for integration of Aadhaar Authentication',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
+  // GitHub Pages URL
   url: 'https://ss11051999.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+
+  // Repository name
   baseUrl: '/aadhaar_auth_docs/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CHiPS', // Usually your GitHub org/user name.
-  projectName: 'aadhaar_auth_docs', // Usually your repo name.
+  // GitHub repository configuration
+  organizationName: 'ss11051999',
+  projectName: 'aadhaar_auth_docs',
 
   trailingSlash: false,
+
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -42,12 +166,14 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+
+          // GitHub repository URL
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ss11051999/aadhaar_auth_docs/tree/main/',
         },
+
         blog: false,
+
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -56,68 +182,82 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+
     colorMode: {
       respectPrefersColorScheme: true,
     },
+
     navbar: {
       title: 'Aadhaar Authentication Documentation',
+
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Aadhaar Authentication Documentation Logo',
         src: 'img/aadhaar_chips_logo.png',
       },
+
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
+
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/ss11051999/aadhaar_auth_docs',
           label: 'GitHub',
           position: 'right',
         },
       ],
     },
+
     footer: {
       style: 'dark',
+
       links: [
         {
           title: 'Docs',
+
           items: [
             {
-              label: 'Tutorial',
+              label: 'Documentation',
               to: '/docs/intro',
             },
           ],
         },
+
         {
-          title: 'Community',
+          title: 'Official Websites',
+
           items: [
             {
               label: 'CHiPS Official Site',
               href: 'https://www.chips.gov.in/en',
             },
+
             {
               label: 'Aadhaar',
               href: 'https://uidai.gov.in/en/',
             },
           ],
         },
+
         {
           title: 'More',
+
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/ss11051999/aadhaar_auth_docs',
             },
           ],
         },
       ],
+
       copyright: `Copyright © ${new Date().getFullYear()} CHiPS`,
     },
+
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
